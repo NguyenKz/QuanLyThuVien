5a5de5d1f921e71cc9d5ffa80a149d2961f0db1c
TRẦN THẢO NGUYÊN <16520857@ms.uit.edu.vn>
2019-06-12 22:28:29 +0700

    Bản đầu tiên

---
+++ frmSuaTheDocGia.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace QLThuVien
+{
+    partial class frmSuaTheDocGia
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.cb_MaDocGia = new System.Windows.Forms.ComboBox();
+            this.cb_LaiDocGia = new System.Windows.Forms.ComboBox();
+            this.tb_HanSuDung = new System.Windows.Forms.TextBox();
+            this.bnt_SuaTheDocGia = new System.Windows.Forms.Button();
+            this.date_NgaySinh = new System.Windows.Forms.DateTimePicker();
+            this.TB_HoVaTen = new System.Windows.Forms.TextBox();
+            this.date_NgayLapThe = new System.Windows.Forms.DateTimePicker();
+            this.tb_DiaChi = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.tb_Email = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lb_HanSuDung = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.cb_MaDocGia);
+            this.panel1.Controls.Add(this.cb_LaiDocGia);
+            this.panel1.Controls.Add(this.tb_HanSuDung);
+            this.panel1.Controls.Add(this.bnt_SuaTheDocGia);
+            this.panel1.Controls.Add(this.date_NgaySinh);
+            this.panel1.Controls.Add(this.TB_HoVaTen);
+            this.panel1.Controls.Add(this.date_NgayLapThe);
+            this.panel1.Controls.Add(this.tb_DiaChi);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.tb_Email);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.lb_HanSuDung);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(13, -2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(327, 371);
+            this.panel1.TabIndex = 1;
+            // 
+            // cb_MaDocGia
+            // 
+            this.cb_MaDocGia.FormattingEnabled = true;
+            this.cb_MaDocGia.Location = new System.Drawing.Point(108, 22);
+            this.cb_MaDocGia.Name = "cb_MaDocGia";
+            this.cb_MaDocGia.Size = new System.Drawing.Size(200, 21);
+            this.cb_MaDocGia.TabIndex = 6;
+            this.cb_MaDocGia.SelectedIndexChanged += new System.EventHandler(this.cb_MaDocGia_SelectedIndexChanged);
+            // 
+            // cb_LaiDocGia
+            // 
+            this.cb_LaiDocGia.FormattingEnabled = true;
+            this.cb_LaiDocGia.Location = new System.Drawing.Point(108, 197);
+            this.cb_LaiDocGia.Name = "cb_LaiDocGia";
+            this.cb_LaiDocGia.Size = new System.Drawing.Size(200, 21);
+            this.cb_LaiDocGia.TabIndex = 6;
+            // 
+            // tb_HanSuDung
+            // 
+            this.tb_HanSuDung.Location = new System.Drawing.Point(108, 230);
+            this.tb_HanSuDung.Name = "tb_HanSuDung";
+            this.tb_HanSuDung.Size = new System.Drawing.Size(200, 20);
+            this.tb_HanSuDung.TabIndex = 4;
+            this.tb_HanSuDung.Text = "5";
+            // 
+            // bnt_SuaTheDocGia
+            // 
+            this.bnt_SuaTheDocGia.Location = new System.Drawing.Point(133, 309);
+            this.bnt_SuaTheDocGia.Name = "bnt_SuaTheDocGia";
+            this.bnt_SuaTheDocGia.Size = new System.Drawing.Size(75, 23);
+            this.bnt_SuaTheDocGia.TabIndex = 3;
+            this.bnt_SuaTheDocGia.Text = "Sửa thẻ";
+            this.bnt_SuaTheDocGia.UseVisualStyleBackColor = true;
+            this.bnt_SuaTheDocGia.Click += new System.EventHandler(this.bnt_SuaTheDocGia_Click);
+            // 
+            // date_NgaySinh
+            // 
+            this.date_NgaySinh.Location = new System.Drawing.Point(108, 90);
+            this.date_NgaySinh.Name = "date_NgaySinh";
+            this.date_NgaySinh.Size = new System.Drawing.Size(200, 20);
+            this.date_NgaySinh.TabIndex = 2;
+            // 
+            // TB_HoVaTen
+            // 
+            this.TB_HoVaTen.Location = new System.Drawing.Point(108, 57);
+            this.TB_HoVaTen.Margin = new System.Windows.Forms.Padding(2);
+            this.TB_HoVaTen.Name = "TB_HoVaTen";
+            this.TB_HoVaTen.Size = new System.Drawing.Size(200, 20);
+            this.TB_HoVaTen.TabIndex = 0;
+            this.TB_HoVaTen.Text = "Tran Van A";
+            // 
+            // date_NgayLapThe
+            // 
+            this.date_NgayLapThe.Location = new System.Drawing.Point(108, 265);
+            this.date_NgayLapThe.Name = "date_NgayLapThe";
+            this.date_NgayLapThe.Size = new System.Drawing.Size(200, 20);
+            this.date_NgayLapThe.TabIndex = 2;
+            // 
+            // tb_DiaChi
+            // 
+            this.tb_DiaChi.Location = new System.Drawing.Point(108, 160);
+            this.tb_DiaChi.Margin = new System.Windows.Forms.Padding(2);
+            this.tb_DiaChi.Name = "tb_DiaChi";
+            this.tb_DiaChi.Size = new System.Drawing.Size(200, 20);
+            this.tb_DiaChi.TabIndex = 0;
+            this.tb_DiaChi.Text = "HCM";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(7, 25);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(61, 13);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "Mã độc giả";
+            // 
+            // tb_Email
+            // 
+            this.tb_Email.Location = new System.Drawing.Point(108, 125);
+            this.tb_Email.Margin = new System.Windows.Forms.Padding(2);
+            this.tb_Email.Name = "tb_Email";
+            this.tb_Email.Size = new System.Drawing.Size(200, 20);
+            this.tb_Email.TabIndex = 0;
+            this.tb_Email.Text = "12345@gmail.com";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(7, 271);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(67, 13);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Ngày lập thẻ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 129);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Email";
+            // 
+            // lb_HanSuDung
+            // 
+            this.lb_HanSuDung.AutoSize = true;
+            this.lb_HanSuDung.Location = new System.Drawing.Point(7, 233);
+            this.lb_HanSuDung.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lb_HanSuDung.Name = "lb_HanSuDung";
+            this.lb_HanSuDung.Size = new System.Drawing.Size(71, 13);
+            this.lb_HanSuDung.TabIndex = 1;
+            this.lb_HanSuDung.Text = "Hạn sử dụng:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 57);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Họ và tên";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(7, 200);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(66, 13);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Loại độc giả";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 164);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Địa chỉ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 90);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ngày sinh";
+            // 
+            // frmSuaTheDocGia
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(353, 389);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "frmSuaTheDocGia";
+            this.Text = "Sửa thẻ độc giả";
+            this.Load += new System.EventHandler(this.frmSuaTheDocGia_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox tb_HanSuDung;
+        private System.Windows.Forms.Button bnt_SuaTheDocGia;
+        private System.Windows.Forms.DateTimePicker date_NgaySinh;
+        private System.Windows.Forms.TextBox TB_HoVaTen;
+        private System.Windows.Forms.DateTimePicker date_NgayLapThe;
+        private System.Windows.Forms.TextBox tb_DiaChi;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox tb_Email;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lb_HanSuDung;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cb_LaiDocGia;
+        private System.Windows.Forms.ComboBox cb_MaDocGia;
+    }
+}
